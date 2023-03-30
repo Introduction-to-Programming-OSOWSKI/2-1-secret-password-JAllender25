f7769bc9acfd426959714113fe088cbe868e4cac
JAllender25 <826735@mystma.org>
2023-03-30 15:00:03 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+def password():
+    enteredPassword = input("what is the password?")
+
+    if enteredPassword == "butterfly":
+        print("ACCESS GRANTED")
+
+    else:
+        print("ACCESS DENIED")
+    password()
+
+password()
